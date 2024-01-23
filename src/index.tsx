c14178413dc7dc38c3ef989707cb9b3d097b112a
drk-knght <omagarwal86@gmail.com>
2024-01-22 21:07:37 -0500

    modified lab and front page

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 
-/*
+
 import React from 'react';
+/*
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -20,4 +21,4 @@ to log results (for example: reportWebVitals(console.log))
 or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 */
-export {};
+// export {};
