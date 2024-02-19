9fee7dce10ef857832089de889ce522106f67f41
drk-knght <omagarwal86@gmail.com>
2024-02-19 15:01:34 -0500

    added responsive design

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -30,56 +30,71 @@ function Assignments(){
                             </div>
                         </div>
                         <hr/>
-                        <ul className="list-group wd-modules mt-4">
+                        <ul className="list-group wd-modules me-4 mt-4">
                             <li className="list-group-items">
                                 <div className="module-header py-3">
                                     <span className="me-2 ms-1">
-                                        <FaEllipsisV className="fa fa-ellipsis-v fs-20" style={{marginRight:-10}}/>
-                                        <FaEllipsisV className="fa fa-ellipsis-v fs-20 ml-n3"/>
+                                        <FaEllipsisV className="fs-20"/>
+                                        <FaEllipsisV className="fs-20" style={{marginLeft:-13}}/>
                                     </span>
                                     <div className="d-inline-flex align-items-center justify-content-center">
-                                        <button className="btn dropdown-toggle me-2"></button>
+                                        <button className="btn dropdown-toggle me-2"/>
                                         <span className="fw-bold cursor-pointer">Assignments</span>
                                     </div>
                                     <span className="float-end pe-3">
-                                        <span className="border border-dark-subtle rounded-pill p-2 fs-12">40% of
-                                            Total</span>
-                                            <FaPlus className="fa fa-plus ms-1 me-2 cursor-pointer"/>
-                                            <FaEllipsisV className="fa fa-ellipsis-v ms-2 fs-20 cursor-pointer"/>
+                                        <span className="border-dark-subtle rounded-pill fs-12 border p-2">
+                                            40% of Total
+                                        </span>
+                                        <i className="me-2 ms-1 cursor-pointer"/>
+                                        <FaEllipsisV className="fs-20 ms-2 cursor-pointer"/>
                                     </span>
                                 </div>
                                 <ul className="list-group">
-                                    {assignmentList.map((assignment)=>(
-                                        <li className="list-group-items assignment-li">
-                                            <div className="module-content fw-bold py-2 d-flex justify-content-center align-items-center ms-1">
-                                                <FaEllipsisV className="fs-20" style={{marginRight:-13}}/>
-                                                <FaEllipsisV className="fs-20"/>
-                                                <PiNotePencil className="mx-4 text-success fs-24"/>
-                                                <span className="me-auto">
-                                                    <Link 
-                                                    to= {`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
-                                                    className="link-dark link-underline link-underline-opacity-0 link-underline-opacity-100-hover">
-                                                        {assignment.title}
-                                                    </Link>
-                                                    <br />
-                                                    <span className="fw-normal fs-12">Week 0 SETUP - Week
-                                                        starting from Monday September 5th (9/5/2022) Module |</span>
-                                                    <br />
-                                                    <span className="fw-normal fs-12"><span className="fw-bold">Due</span> Sep 18,
-                                                        2022 at 11.59pm |
-                                                        100pts
+                                    {assignmentList.map((assignment)=>{
+                                        return (
+                                            <li key={assignment._id} className="list-group-items assignment-li">
+                                                <div className="module-content fw-bold d-flex justify-content-center align-itmes-center ms-1 py-2">
+                                                    <FaEllipsisV className="fs-20"/>
+                                                    <FaEllipsisV className="fs-20" style={{marginLeft:-13}}/>
+                                                    <PiNotePencil className="text-success fs-24 mx-4"/>
+                                                    <span className="w-300 me-auto">
+                                                        <Link 
+                                                        to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
+                                                        className="link-dark link-underline link-underline-opacity-0 link-underline-opacity-100-hover">
+                                                            {assignment.title}
+                                                        </Link>
+                                                        <br/>
+                                                        <Link 
+                                                        to="#"
+                                                        className="fw-normal fs-12 link-danger link-underline link-underline-opacity-0 link-underline-opacity-100-hover">
+                                                            Multiple Modules
+                                                        </Link>
+                                                        <span className="fw-normal fs-16"> | </span>
+                                                        <span className="fw-normal fs-12">
+                                                            {!! assignment.notAvailableBefore && (
+                                                                <span>
+                                                                    <span className="fw-bold">Not available until</span> {" "}{assignment.notAvailableBefore}
+                                                                    <span className="fw-normal fs-16"> | </span>
+
+                                                                </span>
+                                                            )}
+                                                            <span className="fw-bold">Due</span> {assignment.dueDate}
+                                                            <span className="fw-normal fs-16"> | </span> Points: {assignment.totalPoints}
+                                                        </span>
                                                     </span>
-                                                </span>
-                                                <span className="pe-3">
-                                                    <FaCheckCircle className="text-success me-3 fs-20 cursor-pointer"/>
-                                                    <FaEllipsisV className="ms-2 fs-20 cursor-pointer"/>
-                                                </span>
-                                            </div>
-                                        </li>
-                                    ))}
+                                                    <span className="pe-3">
+                                                        <FaCheckCircle className="text-success fs-20 me-3 cursor-pointer"/>
+                                                        <FaEllipsisV className="fs-20 ms-2 cursor-pointer"/>
+                                                    </span>
+
+                                                </div>
+                                            </li>
+                                        );
+                                    })}
                                 </ul>
                             </li>
                         </ul>
+                        
                     </div>
                 </div>
             </>
--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,4 +1,5 @@
-import {Link, useLocation} from "react-router-dom";
+import {Link, useLocation, useParams} from "react-router-dom";
+import { courses } from "../../Database";
 import "./index.css"
  function CourseNavigation(){
     const links=[
@@ -11,15 +12,23 @@ import "./index.css"
             ];
     
     const {pathname}=useLocation();
-
+    const {courseId}=useParams();
+    const course=courses.find(course =>course._id === courseId);
     return (
-        <ul className="d-none d-md-block wd-navigation">
-            {links.map((link,index) =>(
-                <li key={index} className={pathname.includes(encodeURI(link)) ? "wd-active":""}>
-                    <Link to={link}>{link}</Link>
-                </li>
-            ))}
-        </ul>
+        <div>
+            <div className="d-none d-md-block">
+                <div className="ps-5 mb-3 w-10 fs-12"><em className="course-description">{course?.description}</em></div>
+            
+                <ul className="wd-navigation">
+                    {links.map((link,index) =>(
+                        <li key={index} className={pathname.includes(encodeURI(link)) ? "wd-active":""}>
+                            <Link to={link}>{link}</Link>
+                        </li>
+                    ))}
+                </ul>
+                
+            </div>
+        </div>
     );
  }
 
